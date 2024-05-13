b200b11dccdcdf77874eb9ff55f52b5bd9e7a879
Nichlas Overgaard Laugesen <Nicher1>
2024-05-13 22:54:29 +0200

    added lsp

--- init.lua
+++ init.lua
@@ -742,7 +742,7 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
+  clangd = { filetypes = 'python'},
   -- gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
